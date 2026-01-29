dcb5b768ab187ca23769167a3a7eac8f3e3491f7
Rudra Gupta <144617358+rudra-2027@users.noreply.github.com>
2026-01-29 22:16:25 +0530

    Add optimized solution for Jump Game VI
    Implemented an optimized solution for the Jump Game VI problem using dynamic programming and a deque.

---
+++ Week3/1696_Jump_Game_VI.java
@@ -0,0 +1,23 @@
+//Optimized Solution 
+class Solution {
+    public int maxResult(int[] nums, int k) {
+     int n = nums.length;
+     int[] dp = new int[n];
+     dp[0] = nums[0];
+     Deque<Integer> dq =  new ArrayDeque<>();
+     dq.add(0);
+     for(int i = 1;i<n;i++){
+        if(dq.peekFirst()<i-k){
+            dq.pollFirst();
+        }
+        dp[i]= nums[i]+dp[dq.peekFirst()];
+        while(!dq.isEmpty() && dp[dq.peekLast()]<=dp[i]){
+            dq.pollLast();
+        }
+        dq.addLast(i);
+        
+     }
+     return dp[n-1];
+
+    }
+}
